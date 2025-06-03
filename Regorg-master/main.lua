99e9189981ab135c7da41e96104e96579879a455
Mobile User <mobile@localhost>
2025-06-03 16:13:05 +0200

    save local changes at 2025-06-03 16:13:05

---
+++ Regorg-master/main.lua
@@ -0,0 +1,22 @@
+local gfx = require("graphics.graphics")
+local gp3 = require "audio.gp3"
+function love.load()
+
+    shader = gfx.newShader([[ 
+       
+    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+      vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
+      return pixel * color;
+    }
+    ]])
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.setShader(shader)
+    gfx.rect(10, 0, 50, 10, 255, 0)
+    love.graphics.setShader()
+end
