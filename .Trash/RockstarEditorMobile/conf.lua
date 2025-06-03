eb8ef91ae354bae267b99d9846922f83d3e7c3d0
Mobile User <mobile@localhost>
2025-06-03 17:00:31 +0200

    save local changes at 2025-06-03 17:00:31

--- .Trash/RockstarEditorMobile/conf.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-    t.identity = "RockstarEditorLite"
-    t.window.title = "Rockstar Editor Mobile"
-    t.window.fullscreen = false
-    t.window.resizable = true
-    t.modules.joystick = false
-    t.modules.physics = false
-end
